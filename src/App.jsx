bea750880357ebe05c4412364d4fe44fe922e97b
O-Razik <razik8bit@gmail.com>
2025-11-11 13:37:53 +0200

    improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
-import { useState } from 'react'
 import RiskDashboard from './RiskDashboard'
 import './App.css'
 
 function App() {
-  const [showDashboard, setShowDashboard] = useState(false)
-
-  if (showDashboard) {
-    return <RiskDashboard />
-  }
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <button onClick={() => setShowDashboard(true)}>
-        Open Risk Dashboard
-      </button>
+        <RiskDashboard />
     </>
   )
 }
